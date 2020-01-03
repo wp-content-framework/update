4c36952870c97706c70e6ec540b2bd1ca1118f54
Technote <technote.space@gmail.com>
2020-01-03 17:35:45 +0900

    chore: remove version info

--- src/classes/models/update.php
+++ src/classes/models/update.php
@@ -2,7 +2,6 @@
 /**
  * WP_Framework_Update Classes Models Update
  *
- * @version 0.0.8
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
